de3510ee480c465ad0eae5b2cddddd3f50b8de67
FrankoSav <117610367+FrankoSav@users.noreply.github.com>
2023-04-27 14:11:47 +0200

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,41 @@
+#!bin/bash
+
+sudo apt-get install wmctrl
+clear
+
+echo "Youtube Views"
+sleep 5
+clear
+open https://www.youtube.com/watch?v=MuYC7MyThDY
+sleep 5
+xdotool key space
+open https://www.youtube.com/watch?v=MuYC7MyThDY
+sleep 5
+xdotool key space
+open https://www.youtube.com/watch?v=MuYC7MyThDY
+sleep 5
+xdotool key space
+open https://www.youtube.com/watch?v=MuYC7MyThDY
+sleep 5
+xdotool key space
+open https://www.youtube.com/watch?v=MuYC7MyThDY
+sleep 5
+xdotool key space
+open https://www.youtube.com/watch?v=MuYC7MyThDY
+sleep 5
+xdotool key space
+open https://www.youtube.com/watch?v=MuYC7MyThDY
+sleep 5
+xdotool key space
+open https://www.youtube.com/watch?v=MuYC7MyThDY
+sleep 5
+xdotool key space
+open https://www.youtube.com/watch?v=MuYC7MyThDY
+sleep 5
+xdotool key space
+open https://www.youtube.com/watch?v=MuYC7MyThDY
+sleep 5
+xdotool key space
+open https://www.youtube.com/watch?v=MuYC7MyThDY
+sleep 10
+clear
